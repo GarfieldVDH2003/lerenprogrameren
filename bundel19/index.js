4dfc1f2572df87eb97b18011f8c68d32462ddf49
GarfieldVDH2003 <rubenvanderheere1@gmail.com>
2024-09-27 11:06:02 +0200

    oefening 1

--- bundel19/index.js
+++ bundel19/index.js
@@ -1,5 +1,6 @@
 let getal=parseInt(prompt("geef een getal in"));
-for(let i=1;i>=getal;){
-    getal / i;
-    console.log(i);
+for(let i=1;i<=getal;i++){
+    if(i%5===0){
+        console.log(i);
+    }
 }
